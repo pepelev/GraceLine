88437eadff9e440054e3eb80f4de5670cc7be7bf
pepelev <kran440@gmail.com>
2022-08-08 21:21:34 +0500

    Moves WholeInput into separate file

--- GraceLine/Text/Segment.cs
+++ GraceLine/Text/Segment.cs
@@ -177,18 +177,4 @@ namespace GraceLine.Text
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
-
-    public sealed class WholeInput : Source
-    {
-        private readonly string content;
-
-        public WholeInput(string content)
-        {
-            this.content = content;
-        }
-
-        public override string Name => nameof(WholeInput);
-        public override string Content => content;
-        public override string ToString() => content;
-    }
 }
---
+++ GraceLine/Text/WholeInput.cs
@@ -0,0 +1,16 @@
+﻿namespace GraceLine.Text
+{
+    public sealed class WholeInput : Source
+    {
+        private readonly string content;
+
+        public WholeInput(string content)
+        {
+            this.content = content;
+        }
+
+        public override string Name => nameof(WholeInput);
+        public override string Content => content;
+        public override string ToString() => content;
+    }
+}
